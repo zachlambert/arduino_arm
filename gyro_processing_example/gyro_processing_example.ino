bfab442246dc28a277ab5ae57979f000e5c93fdd
Zach Lambert <zachalambert@gmail.com>
2019-09-28 22:57:24 +0100

    Updated some gyro code.

--- gyro_processing_example/gyro_processing_example.ino
+++ gyro_processing_example/gyro_processing_example.ino
@@ -91,7 +91,7 @@ MPU6050 mpu;
 // (in degrees) calculated from the quaternions coming from the FIFO.
 // Note that Euler angles suffer from gimbal lock (for more info, see
 // http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_EULER
+#define OUTPUT_READABLE_EULER
 
 // uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
 // pitch/roll angles (in degrees) calculated from the quaternions coming
@@ -105,7 +105,7 @@ MPU6050 mpu;
 // not compensated for orientation, so +X is always +X according to the
 // sensor, just without the effects of gravity. If you want acceleration
 // compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
-#define OUTPUT_READABLE_REALACCEL
+//#define OUTPUT_READABLE_REALACCEL
 
 // uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
 // components with gravity removed and adjusted for the world frame of
--- gyro_test/gyro_test.ino
+++ gyro_test/gyro_test.ino
@@ -1,19 +1,11 @@
 
 #include <Wire.h>
 #include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
 
 //MPU
 
 const int MPU_ADDR = 0x68;
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
-
-//Radio
-
-RF24 radio(9, 8); //CE, CSN
-
-const byte address[6] = "00001";
 
 void setup() {
   //MPU
@@ -22,19 +14,12 @@ void setup() {
   Wire.write(0x6B);
   Wire.write(0);
   Wire.endTransmission(true);
-
-  //Radio
-  radio.begin();
-  radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN);
-  radio.stopListening();
-
+  
   //Serial
   Serial.begin(9600);
 }
 
 void loop() {
-
   //Read from MPU
 
   Wire.beginTransmission(MPU_ADDR);
@@ -42,37 +27,23 @@ void loop() {
   Wire.endTransmission(false);
   Wire.requestFrom(MPU_ADDR, 12, true);
 
-  AcX = Wire.read()<<8 | Wire.read();
-  AcY = Wire.read()<<8 | Wire.read();  
-  AcZ = Wire.read()<<8 | Wire.read();  
-  GyX = Wire.read()<<8 | Wire.read();  
-  GyY = Wire.read()<<8 | Wire.read();  
-  GyZ = Wire.read()<<8 | Wire.read(); 
+  AcX = (Wire.read()<<8 | Wire.read()) / 1;
+  AcY = (Wire.read()<<8 | Wire.read()) / 1;  
+  AcZ = (Wire.read()<<8 | Wire.read()) / 1;  
+  GyX = (Wire.read()<<8 | Wire.read()) / 131;  
+  GyY = (Wire.read()<<8 | Wire.read()) / 131;  
+  GyZ = (Wire.read()<<8 | Wire.read()) / 131; 
 
-  //Print to Serial
 
-  Serial.print("Accelerometer: ");
-  Serial.print("X = ");
+  
+  //Print to Serial 
+  
   Serial.print(AcX);
-  Serial.print(", Y = ");
+  Serial.print(", ");
   Serial.print(AcY);
-  Serial.print(", Z = ");
+  Serial.print(", ");
   Serial.print(AcZ);
   Serial.println("");
 
-  Serial.print("Gyroscope: ");
-  Serial.print("X = ");
-  Serial.print(GyX);
-  Serial.print(", Y = ");
-  Serial.print(GyY);
-  Serial.print(", Z = ");
-  Serial.print(GyZ);
-  Serial.println("");
-
-  //Write to radio
-
-  const char text[] = "Hello World";
-  radio.write(&text, sizeof(text));
-  
   delay(200);
 }
